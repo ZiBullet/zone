7fc2ed34b88bd17374e0ed3c5c912069450ff6e7
ZiBullet <zamolidinziadulaev@gmail.com>
2024-07-28 18:08:53 +0500

    banner - done

--- js/main.js
+++ js/main.js
@@ -17,8 +17,27 @@ class Fill {
 
 const fill = new Fill({
     btn: ".header__btn"
-})
+});
 
 const bannerFill = new Fill({
     btn: ".banner__join"
+});
+
+class Bubbling {
+    constructor(obj) {
+        this.item = document.querySelector(obj.item);
+
+        window.addEventListener("scroll", () => this.scrollDown(this.item))
+    }
+
+    scrollDown(item) {
+        window.innerWidth > 1175 
+        ? item.style.transform = `translateY(-${window.scrollY * .35}px)`
+        : item.style.transform = `translateY(-${window.scrollY * .15}px)`
+    }
+
+}
+
+const bubbling = new Bubbling({
+    item: ".banner__img"
 })
